8add30a022c28d9a08a2250daab24413d0eeb730
Euan Lacy <euan.bateslacy@protonmail.com>
2022-03-23 17:52:26 +0000

    add further single threaded tests

--- tests/test_queue.cpp
+++ tests/test_queue.cpp
@@ -17,23 +17,60 @@ TEMPLATE_TEST_CASE("Single Threaded Tests", "[ints]", CQueue<int>) {
     static_assert(Queue<TestType, int>);
     auto queue = TestType();
 
-    queue.enqueue(3);
-    auto test = queue.dequeue();
+    SECTION("Dequeue while empty") {
+        auto test = queue.dequeue();
 
-    REQUIRE(*test == 3);
+        REQUIRE(!test.has_value());
+    }
+
+    SECTION("Enqueue Dequeue") {
+        queue.enqueue(3);
+        auto test = queue.dequeue();
+
+        REQUIRE(*test == 3);
+    }
+
+    SECTION("Enqueue Dequeue Dequeue") {
+        queue.enqueue(14);
+
+        auto fourteen = queue.dequeue();
+        REQUIRE(*fourteen == 14);
+
+        auto empty = queue.dequeue();
+        REQUIRE(!empty.has_value());
+    }
+
+    SECTION("Dequeue Enqueue Dequeue") {
+        auto empty = queue.dequeue();
+        REQUIRE(!empty.has_value());
+
+        queue.enqueue(152);
+
+        auto one_five_two = queue.dequeue();
+        REQUIRE(*one_five_two == 152);
+    }
+
+    // Enqueues 100 random numbers in [1, 500), then checks they dequeue in the correct order.
+    SECTION("FIFO Ordering") {
+        auto numbers = GENERATE(take(100, random(1, 500)));
+
+        queue.enqueue(numbers);
+        auto values = queue.dequeue();
+
+        REQUIRE(*values == numbers);
+    }
 }
 
-TEST_CASE("2 Threads") {
+TEST_CASE("Many Threads") {
     CQueue<uint32_t> queue {};
 
     auto fut  = std::async([&queue]{ queue.enqueue(3); });
+    auto fut1 = std::async([&queue]{ queue.enqueue(20); });
+    auto fut2 = std::async([&queue]{ queue.enqueue(12); });
 
     auto fut3 = std::async([&queue]{ return *queue.dequeue(); });
     auto fut4 = std::async([&queue]{ return *queue.dequeue(); });
     auto fut5 = std::async([&queue]{ return *queue.dequeue(); });
-
-    auto fut1 = std::async([&queue]{ queue.enqueue(20); });
-    auto fut2 = std::async([&queue]{ queue.enqueue(12); });
 
     fut.get(), fut1.get(), fut2.get();
 
